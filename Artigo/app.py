a1ce6c91c650f44b987d50a06aabe7b0c9f76ac0
Guilherme <guilhermeguilhermeoliveira25@hotmail.com>
2024-06-13 22:18:42 +0100

    Update

--- Artigo/app.py
+++ Artigo/app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hQwAvEIT-uEz0xaigHxx8g'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-file_path = os.path.abspath(os.path.join(os.getcwd(), 'D:\\Faculdade\\Mestrado\\2 Semestre\\Arquitetura de Software\\SHOP4UMS\\Artigo\\database','artigo.db'))
+file_path = os.path.abspath(os.path.join(os.getcwd(), 'D:\\Faculdade\\Mestrado\\2 Semestre\\Arquitetura de Software\\SHOP4UMS\\Artigo\\database','artigo.db')) # Sem o caminho especifico nao funciona
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.register_blueprint(artigo_blueprint)
 init_app(app)
